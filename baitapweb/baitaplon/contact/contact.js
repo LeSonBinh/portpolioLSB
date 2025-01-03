f70937e68087dbe73bfa5556f411a84a29c4890c
LeSonBinh <binhvtvnpro@gmail.com>
2025-01-03 13:01:15 +0700

    Add files via upload

---
+++ baitapweb/baitaplon/contact/contact.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function send(){
+    const Name=document.getElementById("Name").value
+    const Email=document.getElementById("Email").value
+    const Message=document.getElementById("tinnhan").value
+    if(Name==""||Email==""||Message=="")
+    {
+        alert("em ghét anh ư");
+    }
+    else{
+        alert(document.getElementById("in").innerText="");
+    }
+}
